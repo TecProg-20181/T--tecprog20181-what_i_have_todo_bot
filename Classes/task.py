9412a5b1e1489d02180e8016eb9d847a909ef8ff
filypsdias <lipao.dias@hotmail.com>
2018-04-29 13:23:46 -0300

    Criando classe task
    Co-authored-by: joao4018 <joao4018@gmail.com>

---
+++ Classes/task.py
@@ -0,0 +1,193 @@
+import db
+from db import Task
+from Classes.connection import Connection
+
+CONNECTION = Connection()
+
+class Tasks():
+    def __init__(self):
+        pass
+
+    def deps_text(self, task, chat, preceed=''):
+        self.text = ''
+        for i in range(len(task.dependencies.split(',')[:-1])):
+            self.line = preceed
+            self.query = db.session.query(db.Task).filter_by(id=int(task.dependencies.split(',')[:-1][i]), chat=chat)
+            self.dep = self.query.one()
+
+            self.icon = '\U0001F195'
+            if self.dep.status == 'DOING':
+                self.icon = '\U000023FA'
+            elif self.dep.status == 'DONE':
+                self.icon = '\U00002611'
+
+            if i + 1 == len(task.dependencies.split(',')[:-1]):
+                self.line += '└── [[{}]] {} {}\n'.format(self.dep.id, self.icon, self.dep.name)
+                self.line += self.deps_text(self.dep, chat, preceed + '    ')
+            else:
+                self.line += '├── [[{}]] {} {}\n'.format(self.dep.id, self.icon, self.dep.name)
+                self.line += self.deps_text(self.dep, chat, preceed + '│   ')
+
+            self.text += self.line
+
+        return self.text
+
+    def treatException(self, task_id, chat):
+        self.query = db.session.query(db.Task).filter_by(id=task_id, chat=chat)
+        try:
+            self.task = self.query.one()
+        except sqlalchemy.orm.exc.NoResultFound:
+            CONNECTION.sendMessage("_404_ Task {} not found x.x".format(task_id), chat)
+            return 1
+        return self.task
+
+        
+    def splitDualInput(self, msg, text):    
+        if msg != '':
+            if len(msg.split(' ', 1)) > 1:
+                text = msg.split(' ', 1)[1]
+            msg = msg.split(' ', 1)[0]
+        return msg, text
+
+    def createTask(self, msg, chat):
+        self.task = db.Task(chat=chat, name=msg, status='TODO', dependencies='', parents='', priority='')
+        db.session.add(self.task)
+        db.session.commit()
+        CONNECTION.sendMessage("New task *TODO* [[{}]] {}".format(self.task.id, self.task.name), chat)
+
+    def duplicateTask(self, task_id, chat):
+        self.task = self.treatException(task_id, chat)
+        if self.task == 1:
+            return
+
+        self.dtask = db.Task(chat=self.task.chat, name=self.task.name, status=self.task.status, dependencies=self.task.dependencies,
+                        parents=self.task.parents, priority=self.task.priority, duedate=self.task.duedate)
+        db.session.add(self.dtask)
+
+        for t in self.task.dependencies.split(',')[:-1]:
+            self.qy = db.session.query(db.Task).filter_by(id=int(t), chat=chat)
+            t = self.qy.one()
+            t.parents += '{},'.format(dtask.id)
+
+        db.session.commit()
+        CONNECTION.sendMessage("New task *TODO* [[{}]] {}".format(self.dtask.id, self.dtask.name), chat)
+
+    def moveTask(self, command, task_id, chat):
+        self.task = self.treatException(task_id, chat)
+        if self.task == 1:
+            return
+        self.task.status = command.upper()[1:]
+        db.session.commit()
+        CONNECTION.sendMessage("*"+self.task.status+"* task [[{}]] {}".format(self.task.id, self.task.name), chat)
+
+    def deleteTask(self, task_id, chat):
+        self.task = self.treatException(task_id, chat)
+        if self.task == 1:
+            return
+            
+        for t in self.task.dependencies.split(',')[:-1]:
+            self.qy = db.session.query(db.Task).filter_by(id=int(t), chat=chat)
+            t = self.qy.one()
+            t.parents = t.parents.replace('{},'.format(self.task.id), '')
+        db.session.delete(self.task)
+        db.session.commit()
+        CONNECTION.sendMessage("Task [[{}]] deleted".format(task_id), chat)
+
+    def showTaskList(self, chat):
+        self.a = ''
+
+        self.a += '\U0001F4CB Task List\n'
+        self.query = db.session.query(db.Task).filter_by(parents='', chat=chat).order_by(db.Task.id)
+        for self.task in self.query.all():
+            self.icon = '\U0001F195'
+            if self.task.status == 'DOING':
+                self.icon = '\U000023FA'
+            elif self.task.status == 'DONE':
+                self.icon = '\U00002611'
+
+            self.a += '[[{}]] {} {}\n'.format(self.task.id, self.icon, self.task.name)
+            self.a += self.deps_text(self.task, chat)
+
+        CONNECTION.sendMessage(self.a, chat)
+        self.a = ''
+
+        self.a += '\U0001F4DD _Status_\n'
+        self.query = db.session.query(db.Task).filter_by(status='TODO', chat=chat).order_by(db.Task.id)
+        self.a += '\n\U0001F195 *TODO*\n'
+        for task in self.query.all():
+            self.a += '[[{}]] {}\n'.format(self.task.id, self.task.name)
+        self.query = db.session.query(db.Task).filter_by(status='DOING', chat=chat).order_by(db.Task.id)
+        self.a += '\n\U000023FA *DOING*\n'
+        for task in self.query.all():
+            self.a += '[[{}]] {}\n'.format(self.task.id, self.task.name)
+        self.query = db.session.query(db.Task).filter_by(status='DONE', chat=chat).order_by(db.Task.id)
+        self.a += '\n\U00002611 *DONE*\n'
+        for task in self.query.all():
+            self.a += '[[{}]] {}\n'.format(self.task.id, self.task.name)
+
+        CONNECTION.sendMessage(self.a, chat)
+
+    def dependson(self, text, task_id, chat):
+        self.task = self.treatException(task_id, chat)
+        if self.task == 1:
+            return
+
+        if text == '':
+            for i in self.task.dependencies.split(',')[:-1]:
+                i = int(i)
+                self.q = db.session.query(db.Task).filter_by(id=i, chat=chat)
+                self.t = self.q.one()
+                self.t.parents = self.t.parents.replace('{},'.format(self.task.id), '')
+
+            self.task.dependencies = ''
+            CONNECTION.sendMessage("Dependencies removed from task {}".format(task_id), chat)
+        else:
+            for depid in text.split(' '):
+                if not depid.isdigit():
+                    CONNECTION.sendMessage("All dependencies ids must be numeric, and not {}".format(depid), chat)
+                else:
+                    depid = int(depid)
+                    self.query = db.session.query(db.Task).filter_by(id=depid, chat=chat)
+                    try:
+                        self.taskdep = self.query.one()
+                        self.taskdep.parents += str(self.task.id) + ','
+                    except sqlalchemy.orm.exc.NoResultFound:
+                        CONNECTION.sendMessage("_404_ Task {} not found x.x".format(depid), chat)
+                        continue
+
+                    self.deplist = self.task.dependencies.split(',')
+                    if str(depid) not in self.deplist:
+                        self.task.dependencies += str(depid) + ','
+
+        db.session.commit()
+        CONNECTION.sendMessage("Task {} dependencies up to date".format(task_id), chat)
+
+    def renameTask(self, text, task_id, chat):
+        self.task = self.treatException(task_id, chat)
+        if self.task == 1:
+            return
+
+        if text == '':
+            CONNECTION.sendMessage("You want to modify task {}, but you didn't provide any new text".format(task_id), chat)
+            return
+                        
+        self.old_text = self.task.name
+        self.task.name = text
+        db.session.commit()
+        CONNECTION.sendMessage("Task {} redefined from {} to {}".format(task_id, self.old_text, text), chat)
+
+    def priorityTask(self, text, task_id, chat):
+        self.task = self.treatException(task_id, chat)
+        if self.task == 1:
+            return
+
+        if text == '':
+            self.task.priority = ''
+            CONNECTION.sendMessage("_Cleared_ all priorities from task {}".format(task_id), chat)
+        else:
+            if text.lower() not in ['high', 'medium', 'low']:
+                CONNECTION.sendMessage("The priority *must be* one of the following: high, medium, low", chat)
+            else:
+                self.task.priority = text.lower()
+                CONNECTION.sendMessage("*Task {}* priority has priority *{}*".format(task_id, text.lower()), chat)
+        db.session.commit() 
--- taskbot.py
+++ taskbot.py
@@ -3,6 +3,7 @@
 import time
 import string
 from Classes.connection import Connection
+from Classes.task import Tasks
 import sqlalchemy
 
 import db
@@ -27,196 +28,20 @@ COMMANDS = [' /new ', ' /todo ', ' /doing ', ' /done ',
            ' /dependson ', ' /duplicate ', ' /priority ']
 
 CONNECTION = Connection()
+TASK = Tasks()
 
 def get_last_update_id(updates):
     update_ids = []
     for update in updates["result"]:
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
-
-def deps_text(task, chat, preceed=''):
-    text = ''
-
-    for i in range(len(task.dependencies.split(',')[:-1])):
-        line = preceed
-        query = db.session.query(Task).filter_by(id=int(task.dependencies.split(',')[:-1][i]), chat=chat)
-        dep = query.one()
-
-        icon = '\U0001F195'
-        if dep.status == 'DOING':
-            icon = '\U000023FA'
-        elif dep.status == 'DONE':
-            icon = '\U00002611'
-
-        if i + 1 == len(task.dependencies.split(',')[:-1]):
-            line += '└── [[{}]] {} {}\n'.format(dep.id, icon, dep.name)
-            line += deps_text(dep, chat, preceed + '    ')
-        else:
-            line += '├── [[{}]] {} {}\n'.format(dep.id, icon, dep.name)
-            line += deps_text(dep, chat, preceed + '│   ')
-
-        text += line
-
-    return text
-
-def treatException(task_id, chat):
-    query = db.session.query(Task).filter_by(id=task_id, chat=chat)
-    try:
-        task = query.one()
-    except sqlalchemy.orm.exc.NoResultFound:
-        CONNECTION.sendMessage("_404_ Task {} not found x.x".format(task_id), chat)
-        return 1
-    return task
+    
 def splitDualInput(msg, text):    
     if msg != '':
         if len(msg.split(' ', 1)) > 1:
             text = msg.split(' ', 1)[1]
         msg = msg.split(' ', 1)[0]
     return msg, text
-
-def createTask(msg, chat):
-    task = Task(chat=chat, name=msg, status='TODO', dependencies='', parents='', priority='')
-    db.session.add(task)
-    db.session.commit()
-    CONNECTION.sendMessage("New task *TODO* [[{}]] {}".format(task.id, task.name), chat)
-
-def duplicateTask(task_id, chat):
-    task = treatException(task_id, chat)
-    if task == 1:
-        return
-
-    dtask = Task(chat=task.chat, name=task.name, status=task.status, dependencies=task.dependencies,
-                    parents=task.parents, priority=task.priority, duedate=task.duedate)
-    db.session.add(dtask)
-
-    for t in task.dependencies.split(',')[:-1]:
-        qy = db.session.query(Task).filter_by(id=int(t), chat=chat)
-        t = qy.one()
-        t.parents += '{},'.format(dtask.id)
-
-    db.session.commit()
-    CONNECTION.sendMessage("New task *TODO* [[{}]] {}".format(dtask.id, dtask.name), chat)
-
-def moveTask(command, task_id, chat):
-    task = treatException(task_id, chat)
-    if task == 1:
-        return
-    task.status = command.upper()[1:]
-    db.session.commit()
-    CONNECTION.sendMessage("*"+task.status+"* task [[{}]] {}".format(task.id, task.name), chat)
-
-def deleteTask(task_id, chat):
-
-    task = treatException(task_id, chat)
-    if task == 1:
-        return
-        
-    for t in task.dependencies.split(',')[:-1]:
-        qy = db.session.query(Task).filter_by(id=int(t), chat=chat)
-        t = qy.one()
-        t.parents = t.parents.replace('{},'.format(task.id), '')
-    db.session.delete(task)
-    db.session.commit()
-    CONNECTION.sendMessage("Task [[{}]] deleted".format(task_id), chat)
-
-def showTaskList(chat):
-    a = ''
-
-    a += '\U0001F4CB Task List\n'
-    query = db.session.query(Task).filter_by(parents='', chat=chat).order_by(Task.id)
-    for task in query.all():
-        icon = '\U0001F195'
-        if task.status == 'DOING':
-            icon = '\U000023FA'
-        elif task.status == 'DONE':
-            icon = '\U00002611'
-
-        a += '[[{}]] {} {}\n'.format(task.id, icon, task.name)
-        a += deps_text(task, chat)
-
-    CONNECTION.sendMessage(a, chat)
-    a = ''
-
-    a += '\U0001F4DD _Status_\n'
-    query = db.session.query(Task).filter_by(status='TODO', chat=chat).order_by(Task.id)
-    a += '\n\U0001F195 *TODO*\n'
-    for task in query.all():
-        a += '[[{}]] {}\n'.format(task.id, task.name)
-    query = db.session.query(Task).filter_by(status='DOING', chat=chat).order_by(Task.id)
-    a += '\n\U000023FA *DOING*\n'
-    for task in query.all():
-        a += '[[{}]] {}\n'.format(task.id, task.name)
-    query = db.session.query(Task).filter_by(status='DONE', chat=chat).order_by(Task.id)
-    a += '\n\U00002611 *DONE*\n'
-    for task in query.all():
-        a += '[[{}]] {}\n'.format(task.id, task.name)
-
-    CONNECTION.sendMessage(a, chat)
-
-def dependson(text, task_id, chat):
-    task = treatException(task_id, chat)
-    if task == 1:
-        return
-
-    if text == '':
-        for i in task.dependencies.split(',')[:-1]:
-            i = int(i)
-            q = db.session.query(Task).filter_by(id=i, chat=chat)
-            t = q.one()
-            t.parents = t.parents.replace('{},'.format(task.id), '')
-
-        task.dependencies = ''
-        CONNECTION.sendMessage("Dependencies removed from task {}".format(task_id), chat)
-    else:
-        for depid in text.split(' '):
-            if not depid.isdigit():
-                CONNECTION.sendMessage("All dependencies ids must be numeric, and not {}".format(depid), chat)
-            else:
-                depid = int(depid)
-                query = db.session.query(Task).filter_by(id=depid, chat=chat)
-                try:
-                    taskdep = query.one()
-                    taskdep.parents += str(task.id) + ','
-                except sqlalchemy.orm.exc.NoResultFound:
-                    CONNECTION.sendMessage("_404_ Task {} not found x.x".format(depid), chat)
-                    continue
-
-                deplist = task.dependencies.split(',')
-                if str(depid) not in deplist:
-                    task.dependencies += str(depid) + ','
-
-    db.session.commit()
-    CONNECTION.sendMessage("Task {} dependencies up to date".format(task_id), chat)
-
-def renameTask(text, task_id, chat):
-    task = treatException(task_id, chat)
-    if task == 1:
-        return
-
-    if text == '':
-        CONNECTION.sendMessage("You want to modify task {}, but you didn't provide any new text".format(task_id), chat)
-        return
-                    
-    old_text = task.name
-    task.name = text
-    db.session.commit()
-    CONNECTION.sendMessage("Task {} redefined from {} to {}".format(task_id, old_text, text), chat)
-
-def priorityTask(text, task_id, chat):
-    task = treatException(task_id, chat)
-    if task == 1:
-        return
-
-    if text == '':
-        task.priority = ''
-        CONNECTION.sendMessage("_Cleared_ all priorities from task {}".format(task_id), chat)
-    else:
-        if text.lower() not in ['high', 'medium', 'low']:
-            CONNECTION.sendMessage("The priority *must be* one of the following: high, medium, low", chat)
-        else:
-            task.priority = text.lower()
-            CONNECTION.sendMessage("*Task {}* priority has priority *{}*".format(task_id, text.lower()), chat)
-    db.session.commit() 
 
 def gettingMessage(update):
     if 'message' in update:
@@ -251,10 +76,10 @@ def handle_updates(updates):
             return
 
         elif command == '/new':
-            createTask(msg, chat)
+            TASK.createTask(msg, chat)
 
         elif command == '/list':
-            showTaskList(chat)
+            TASK.showTaskList(chat)
 
         elif command == '/start':
             CONNECTION.sendMessage("Welcome! Here is a list of things you can do.", chat)
@@ -277,28 +102,28 @@ def handle_updates(updates):
                 task_id = int(msg)
 
                 if command == '/duplicate':
-                    duplicateTask(task_id, chat)
+                    TASK.duplicateTask(task_id, chat)
 
                 elif command == '/delete':
-                    deleteTask(task_id, chat)
+                    TASK.deleteTask(task_id, chat)
 
                 elif command == '/todo':
-                    moveTask(command, task_id, chat)
+                    TASK.moveTask(command, task_id, chat)
 
                 elif command == '/doing':
-                    moveTask(command, task_id, chat)
+                    TASK.moveTask(command, task_id, chat)
 
                 elif command == '/done':
-                    moveTask(command, task_id, chat)
+                    TASK.moveTask(command, task_id, chat)
 
                 elif command == '/rename':
-                    renameTask(text, task_id, chat)
+                    TASK.renameTask(text, task_id, chat)
 
                 elif command == '/priority':
-                    priorityTask(text, task_id, chat)
+                    TASK.priorityTask(text, task_id, chat)
 
                 elif command == '/dependson':
-                    dependson(text, task_id, chat)
+                    TASK.dependson(text, task_id, chat)
 def main():
     last_update_id = None
     while True:
